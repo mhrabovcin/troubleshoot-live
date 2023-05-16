b8a5c2f86031a4d2b1f65c62c76beffe1d390ab5
Martin Hrabovcin <mhrabovcin.c@d2iq.com>
2023-05-16 22:29:36 +0200

    fix: rewrite priority class and ingress class

--- pkg/rewriter/default.go
+++ pkg/rewriter/default.go
@@ -1,10 +1,31 @@
 package rewriter
 
+import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
 // Default provides a rewriter that covers most cases of required changes for
 // successful import and serving of a diagnostics bundle.
 func Default() ResourceRewriter {
 	return Multi(
 		GeneratedValues(),
 		DeletedNamespace(),
+		When(
+			MatchGVK(schema.FromAPIVersionAndKind("v1", "Pod")),
+			Multi(
+				RemoveField("spec", "priority"),
+				RemoveField("spec", "priorityClassName"),
+			),
+		),
+		When(
+			MatchGVK(schema.FromAPIVersionAndKind("v1", "Pod")),
+			Multi(
+				RemoveField("spec", "runtimeClassName"),
+			),
+		),
+		When(
+			MatchGVK(schema.FromAPIVersionAndKind("networking.k8s.io/v1", "Ingress")),
+			RemoveField("spec", "ingressClassName"),
+		),
 	)
 }
