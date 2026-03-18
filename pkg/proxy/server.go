1a11a2aac9d05cd64d00f529a92e3b9bc4147468
Martin Hrabovcin <mhrabovcin@gmail.com>
2026-03-18 22:05:13 +0100

    chore: add default prefix to bundle proxy (#162)

--- pkg/proxy/server.go
+++ pkg/proxy/server.go
@@ -1,9 +1,11 @@
 package proxy
 
 import (
-	"log"
+	"fmt"
 	"log/slog"
 	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/gorilla/mux"
 	"k8s.io/client-go/rest"
@@ -12,18 +14,64 @@ import (
 	"github.com/mhrabovcin/troubleshoot-live/pkg/rewriter"
 )
 
+// NormalizeHTTPPrefix returns normalized proxy HTTP prefix or empty string.
+func NormalizeHTTPPrefix(prefix string) (string, error) {
+	if prefix == "" {
+		return "", nil
+	}
+
+	u, err := url.Parse(prefix)
+	if err != nil {
+		return "", err
+	}
+	if u.Scheme != "" || u.Host != "" || u.RawQuery != "" || u.Fragment != "" {
+		return "", fmt.Errorf("invalid http prefix %q: must be path only", prefix)
+	}
+
+	p := u.Path
+	if p == "" || p == "/" {
+		return "", nil
+	}
+	if !strings.HasPrefix(p, "/") {
+		p = "/" + p
+	}
+	p = strings.TrimRight(p, "/")
+	if p == "" || p == "/" {
+		return "", nil
+	}
+	return p, nil
+}
+
 // New create new proxy handler that can be used by HTTP library.
-func New(cfg *rest.Config, b bundle.Bundle, rr rewriter.ResourceRewriter) http.Handler {
+func New(cfg *rest.Config, b bundle.Bundle, rr rewriter.ResourceRewriter, httpPrefix string) (http.Handler, error) {
 	proxyHandler, err := ReverseProxyForAPIServerHandler(cfg)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
+
+	prefix, err := NormalizeHTTPPrefix(httpPrefix)
+	if err != nil {
+		return nil, err
+	}
+
 	// disable bodyclose linting as it seems like false positive
 	// https://github.com/timakin/bodyclose/issues/42
 	proxyHandler.ModifyResponse = proxyModifyResponse(rr) //nolint:bodyclose // false positive
 
+	return newRouterWithPrefix(prefix, b, proxyHandler), nil
+}
+
+func newRouterWithPrefix(prefix string, b bundle.Bundle, proxyHandler http.Handler) http.Handler {
 	r := mux.NewRouter()
-	r.Handle("/api/v1/namespaces/{namespace}/pods/{pod}/log", LogsHandler(b, slog.With("handler", "LogsHandler")))
-	r.PathPrefix("/").Handler(proxyHandler)
+	if prefix == "" {
+		r.Handle("/api/v1/namespaces/{namespace}/pods/{pod}/log", LogsHandler(b, slog.With("handler", "LogsHandler")))
+		r.PathPrefix("/").Handler(proxyHandler)
+		return r
+	}
+
+	subrouter := r.PathPrefix(prefix).Subrouter()
+	subrouter.Handle("/api/v1/namespaces/{namespace}/pods/{pod}/log", LogsHandler(b, slog.With("handler", "LogsHandler")))
+	subrouter.PathPrefix("/").Handler(http.StripPrefix(prefix, proxyHandler))
+
 	return r
 }
